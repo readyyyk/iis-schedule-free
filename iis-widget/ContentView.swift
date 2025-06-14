d90b082aefc0b04742044aad7bfe2160a82d5758
readyyyk <mr.sairexer@gmail.com>
2025-06-14 12:02:23 +0300

    Initial Commit

---
+++ iis-widget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iis-widget
+//
+//  Created by Vladislav Puzik on 14.06.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iis-widget/iis_widgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  iis_widgetApp.swift
+//  iis-widget
+//
+//  Created by Vladislav Puzik on 14.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct iis_widgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
